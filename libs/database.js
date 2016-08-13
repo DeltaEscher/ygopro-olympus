4d7d1f7ff2f8d434d08e6d61f434dc6f1c8f008d
Zayelion <panthrowzay@gmail.com>
2016-08-13 19:42:09 -0400

    user objects

--- libs/database.js
+++ libs/database.js
@@ -1,7 +1,35 @@
 /*jslint node:true*/
-
+'use strict';
 var Datastore = require('nedb'),
     db = new Datastore({
         filename: '../database/usersrankings.nedb',
         autoload: true
     });
+
+
+function createRankingBase() {
+    return {
+        wins: 0,
+        loses: 0,
+        draws: 0,
+        elo: 1200,
+        trueskill: 1200,
+        points: 0
+    };
+}
+
+function createNewUser(username) {
+
+    return {
+        username: username,
+        login: '',
+        tcg: createRankingBase(),
+        ocg: createRankingBase(),
+        traditional: createRankingBase(),
+        worlds: createRankingBase(),
+        anime: createRankingBase(),
+        highlander: createRankingBase(),
+        goat: createRankingBase()
+
+    };
+}
