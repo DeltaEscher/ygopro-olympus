45da168b27b90948826ab2282c44d066c8c98c10
Zayelion <panthrowzay@gmail.com>
2016-08-13 20:54:40 -0400

    dataprocessing to fit YGOSharp

--- libs/database.js
+++ libs/database.js
@@ -38,7 +38,9 @@ function createNewUser(username) {
 function prepTrueSkill(ladder, duelist, rank) {
     var output = {
         skill: [],
-        rank: rank
+        rank: rank,
+        ladder: ladder,
+        login: duelist.login
     };
     output.skill = duelist[ladder].trueskill;
     return output;
@@ -52,15 +54,63 @@ function lookup(login, callback) {
     }, callback);
 }
 
-function update(duelist, callback) {
-
-
+function updatePlayerTrueSkill(duelist, callback) {
+    var query = {
+            login: duelist.login
+        },
+        update = {
+            $set: {}
+        };
+    update.$set[duelist.ladder].trueskill = duelist.skill;
+    db.update(query, update, callback);
 }
 
-//var duelresult = {
-//        won :['playername'],
-//        loss : ['playername']
-//    }
+
+/*var = matchorsingleduel{
+    players : ['name', 'name'],
+    result : {
+        winner : 0,
+        method : 2
+    }
+}*/
+/*var = tagduel{
+    players : ['name', 'name', 'name', 'name'],
+    result : {
+        winner : 0,
+        method : 2
+    }
+}*/
+
+function translateDuelResult(duel) {
+    var duelresult = {
+        won: [],
+        loss: []
+    };
+
+    if (duel.players.length === 2) {
+        if (duel.winner === 0) {
+            duelresult.won.push(duel.players[0]);
+            duelresult.loss.push(duel.players[1]);
+        } else {
+            duelresult.won.push(duel.players[1]);
+            duelresult.loss.push(duel.players[0]);
+        }
+    } else if (duel.players.length === 4) {
+        if (duel.winner === 0) {
+            duelresult.won.push(duel.players[0]);
+            duelresult.won.push(duel.players[1]);
+            duelresult.loss.push(duel.players[2]);
+            duelresult.loss.push(duel.players[3]);
+        } else {
+            duelresult.won.push(duel.players[2]);
+            duelresult.won.push(duel.players[3]);
+            duelresult.loss.push(duel.players[0]);
+            duelresult.loss.push(duel.players[1]);
+        }
+    }
+    return duelresult;
+}
+
 function processDuel(duelResult, ladder, callback) {
     var duelistRecords = {
         winners: [],
@@ -76,7 +126,7 @@ function processDuel(duelResult, ladder, callback) {
             skillEngine.push(prepTrueSkill(ladder, duelist, 2));
         });
         trueskill.AdjustPlayers(skillEngine);
-        asyncEach(skillEngine, update, function () {
+        asyncEach(skillEngine, updatePlayerTrueSkill, function () {
             callback();
         });
 
