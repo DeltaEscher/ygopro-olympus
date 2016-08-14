ddda04e47ea655c8df4ff6c5816609e15d910ebc
Zayelion <panthrowzay@gmail.com>
2016-08-14 13:28:14 -0400

    http server with endpoint nubs.

--- libs/httpserver.js
+++ libs/httpserver.js
@@ -1,21 +1,19 @@
 /*jslint node : true*/
 'use strict';
 var express = require('express'),
-
     path = require("path"),
     toobusy = require('toobusy-js'),
     app = express(),
     vhost = require('vhost'),
-    serveIndex = require('serve-index');
+    serveIndex = require('serve-index'),
+    site = process.env.ProductionSITE || 'ygorankings.ml';
 
 function createVirtualStaticHost(domainName, dirPath) {
     return vhost(domainName, express['static'](dirPath));
 }
 
-
-
 app.use(createVirtualStaticHost('localhost', require('path').resolve(process.cwd() + '\\..\\http')));
-app.use(createVirtualStaticHost(process.env.ProductionSITE, require('path').resolve(process.cwd() + '\\..\\http')));
+app.use(createVirtualStaticHost(site, require('path').resolve(process.cwd() + '\\..\\http')));
 
 app.use(function (req, res, next) {
     if (toobusy()) {
@@ -23,6 +21,18 @@ app.use(function (req, res, next) {
     } else {
         next();
     }
+});
+
+app.post('/register', function (req, res) {
+    res.send('POST request for register');
+});
+
+app.post('/updatelogin', function (req, res) {
+    res.send('Post request for update login');
+});
+
+app.get('/ladder', function (req, res) {
+    res.send('GET request for ladder JSON');
 });
 
 app.listen(80);
